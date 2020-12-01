066b5fe35a44ca4e6e4e77870ce38cac4f9ce920
ambar-ml <68280445+ambar-ml@users.noreply.github.com>
2020-12-01 23:00:38 +0530

    Update pred_api.py

--- pred_api.py
+++ pred_api.py
@@ -13,7 +13,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 
 # defing stopwords manually
 
-model=load_model('D:/mod/cnn_toxic_new(1).h5')
+model=load_model('/static/cnn_toxic_new.h5')
 with open('Downloads/tokenizer_toxic.pickle', 'rb') as handle:
     tokenizer_toxic = pickle.load(handle)
 
