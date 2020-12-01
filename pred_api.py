0c6a219b0570107bcde905e7cd7e390a405c40a9
ambar-ml <68280445+ambar-ml@users.noreply.github.com>
2020-12-01 20:35:59 +0530

    Update pred_api.py

--- pred_api.py
+++ pred_api.py
@@ -7,12 +7,13 @@ This is a temporary script file.
 import re
 import pickle
 from nltk.stem.wordnet import WordNetLemmatizer
-from tensorflow.keras.models import load_model
+#from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
+from sklearn.externals import joblib
 # defing stopwords manually
 
-model=load_model('./static/models/cnn_toxic_new.h5')
+model=joblib.load('./static/models/cnn_toxic_new.h5')
 with open('static/models/tokenizer_toxic.pickle', 'rb') as handle:
     tokenizer_toxic = pickle.load(handle)
 
